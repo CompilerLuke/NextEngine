21771db69f546f8d2fe3f42aabfe3ed980c6ac1f
Lucas Goetz <lgoetz@islux.lu>
2020-12-26 14:37:17 +0100

    Forgot to add rhi/async_cpu_copy.h

---
+++ NextEngine/include/graphics/rhi/async_cpu_copy.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "engine/handle.h"
+
+struct AsyncCopyResources;
+struct CommandBuffer;
+
+ENGINE_API AsyncCopyResources* make_async_copy_resources(uint size);
+ENGINE_API void destroy_async_copy_resources(AsyncCopyResources& resources);
+ENGINE_API void async_copy_image(CommandBuffer& cmd_buffer, texture_handle image_handle, AsyncCopyResources& copy);
+ENGINE_API void receive_transfer(AsyncCopyResources& resources, uint size, void* dst);
